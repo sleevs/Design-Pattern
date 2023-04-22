f4f6a01cf67d2529410386d51d6a09aa35b2a02a
Jeison Muniz <jsnprogramer@gmail.com>
2023-04-21 23:52:10 -0300

    implementation of facade pattern

---
+++ structural/FacadePattern.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+class DeviceManager{
+
+   public: void writeDevice();
+   public: void readDevice();
+   public: void listDevice();
+   
+};
+
+int main(){
+    std::cout << "FACADE APP";
+   return 0; 
+}
